b37d774059906cac9a5b895b2112f2c2aeab8d94
Seanp2k <github@seanp2k.endjunk.com>
2010-09-15 21:37:22 -0400

    Revised check-for-pid.sh

--- check-for-pid.sh
+++ check-for-pid.sh
@@ -1,13 +1,18 @@
+!#/bin/bash
+
 if [ -f $0.pid ]; then
-echo -n "Found PID: "
-echo "$0 Already running!  Exiting now..."
-cat $0.pid
-exit 2;
+	echo -n "Found PID: "
+	echo "$0 Already running!  Exiting now..."
+	cat $0.pid
+	exit 2;
 fi
 
 echo $$ > $0.pid
 
-echo did some stuff
+echo 'Do some stuff'
 sleep 5
-echo removing my PID
+
+echo 'removing my PID'
+
 rm $0.pid
+exit 0
